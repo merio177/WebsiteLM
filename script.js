e622002bdfe3e946a72d112ad34001ae8a1ed4d3
gitSomS <rubensilva.contacto@gmail.com>
2023-12-30 21:09:33 +0000

    Update

--- script.js
+++ script.js
@@ -3,46 +3,89 @@ document.addEventListener('DOMContentLoaded', function() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     const ctx = canvas.getContext('2d');
+    const starInfo = document.getElementById('starInfo');
 
-    const stars = [];
+    let stars = [];
 
     for (let i = 0; i < 100; i++) {
-        stars.push(createStar(canvas, ctx, stars));
+        stars.push(createStar(canvas, ctx, stars, starInfo));
     }
+
+    document.addEventListener('click', function(event) {
+        if (!event.target.classList.contains('star')) {
+            starInfo.style.display = 'none';
+        }
+    });
+
+    document.addEventListener('dblclick', function(event) {
+        if (isSpaceFree(event.pageX, event.pageY, stars)) {
+            const newStar = createStar(canvas, ctx, stars, starInfo, event.pageX, event.pageY);
+            stars.push(newStar);
+        }
+    });
 });
 
-function createStar(canvas, ctx, stars) {
+function createStar(canvas, ctx, stars, starInfo, x = Math.random() * canvas.width, y = Math.random() * canvas.height) {
     const star = document.createElement('div');
     star.className = 'star';
-    star.style.left = `${Math.random() * canvas.width}px`;
-    star.style.top = `${Math.random() * canvas.height}px`;
+    star.style.left = `${x}px`;
+    star.style.top = `${y}px`;
     star.style.width = `${Math.random() * 3 + 1}px`;
     star.style.height = star.style.width;
+
+    // Atribuir uma emoção fixa para cada estrela
+    const emotions = ["Feliz", "Triste", "Empolgado", "Pensativo"];
+    const starEmotion = emotions[Math.floor(Math.random() * emotions.length)];
+    star.setAttribute('data-emotion', starEmotion);
 
     star.addEventListener('mouseover', () => {
         drawLines(ctx, stars, star);
     });
 
-    star.onclick = function() {
-        alert('Você clicou em uma estrela!');
-    };
+    star.addEventListener('click', function(event) {
+        event.stopPropagation();
+        starInfo.style.display = 'block';
+        starInfo.style.left = `${event.pageX}px`;
+        starInfo.style.top = `${event.pageY}px`;
+
+        const emotion = star.getAttribute('data-emotion');
+        starInfo.innerHTML = `Informações da Estrela:<br>Coordenada X: ${event.pageX}<br>Coordenada Y: ${event.pageY}<br>Emoção: ${emotion}`;
+    });
 
     document.body.appendChild(star);
     return star;
 }
 
+function isSpaceFree(x, y, stars, minDistance = 50) {
+    return stars.every(star => {
+        const starX = parseInt(star.style.left);
+        const starY = parseInt(star.style.top);
+        const distance = Math.sqrt(Math.pow(starX - x, 2) + Math.pow(starY - y, 2));
+        return distance >= minDistance;
+    });
+}
+
 function drawLines(ctx, stars, currentStar) {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.beginPath();
-    ctx.moveTo(currentStar.offsetLeft + currentStar.offsetWidth / 2, currentStar.offsetTop + currentStar.offsetHeight / 2);
+    const currentEmotion = currentStar.getAttribute('data-emotion');
 
-    const numberOfLines = Math.floor(Math.random() * 5) + 1; // De 1 a 5 linhas
-    for (let i = 0; i < numberOfLines; i++) {
-        const targetStar = stars[Math.floor(Math.random() * stars.length)];
-        ctx.lineTo(targetStar.offsetLeft + targetStar.offsetWidth / 2, targetStar.offsetTop + targetStar.offsetHeight / 2);
+    // Filtra as estrelas com a mesma emoção
+    const connectedStars = stars.filter(star => star.getAttribute('data-emotion') === currentEmotion);
+
+    if (connectedStars.length > 0) {
+        // Começa a desenhar a partir da estrela atual
+        ctx.beginPath();
         ctx.moveTo(currentStar.offsetLeft + currentStar.offsetWidth / 2, currentStar.offsetTop + currentStar.offsetHeight / 2);
-    }
 
-    ctx.strokeStyle = 'white';
-    ctx.stroke();
+        // Desenha linhas para as outras estrelas com a mesma emoção
+        connectedStars.forEach(targetStar => {
+            ctx.lineTo(targetStar.offsetLeft + targetStar.offsetWidth / 2, targetStar.offsetTop + targetStar.offsetHeight / 2);
+        });
+
+        // Volta para a estrela atual para fechar o caminho
+        ctx.lineTo(currentStar.offsetLeft + currentStar.offsetWidth / 2, currentStar.offsetTop + currentStar.offsetHeight / 2);
+
+        ctx.strokeStyle = 'white';
+        ctx.stroke();
+    }
 }
